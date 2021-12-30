453510e59bc2fc119b6a77d36dfea2404c55c0c0
HansenH <HHSWT@hotmail.com>
2021-12-30 21:42:57 +0800

    modify readme

---
+++ examples/example1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"math"
+	"os"
+	"plot-function-svg/plot"
+)
+
+func main() {
+	f := func(x, y float64) float64 {
+		r := math.Hypot(x, y)
+		return math.Sin(r) / r
+	}
+	file, _ := os.Create("example.svg")
+	plot.PlotSVG(f, file, nil)
+}
